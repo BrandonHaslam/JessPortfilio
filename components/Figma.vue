f0947f598176f16499029467868d8312ead07792
BrandonHaslam <BrandonHaslam@users.noreply.github.com>
2022-02-25 23:15:30 +0000

    Header start, tailwind and page styles

---
+++ components/Figma.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <!-- <iframe
+      style="border: 1px solid rgba(0, 0, 0, 0.1)"
+      width="800"
+      height="450"
+      :src="blok.link.url"
+      loading="lazy"
+      allowfullscreen
+    ></iframe> -->
+    <h1>{{ blok.text }}</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: "FigmaFrame",
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
--- components/Page.vue
+++ components/Page.vue
@@ -1,22 +1,40 @@
 <template>
-  <div
-    v-editable="blok"
-    class="px-6">
+  <div v-editable="blok" class="px-6">
     <component
       v-for="blok in blok.body"
       :key="blok._uid"
       :blok="blok"
-      :is="blok.component" />
+      :is="blok.component"
+    />
   </div>
 </template>
  
 <script>
 export default {
+  // data: () => ({}),
   props: {
     blok: {
       type: Object,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+  methods: {
+    setColours() {
+      let body = document.querySelector("body");
+      let storyBlokBackgroundColour = this.blok.BackgroundColour.color;
+      let storyBlokTextColour = this.blok.TextColour.color;
+
+      storyBlokBackgroundColour
+        ? (body.style.background = storyBlokBackgroundColour)
+        : (body.style.background = "white");
+
+      storyBlokTextColour
+        ? (body.style.color = storyBlokTextColour)
+        : (body.style.background = "black");
+    },
+  },
+  mounted() {
+    this.setColours();
+  },
+};
 </script>
