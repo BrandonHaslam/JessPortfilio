2102d2834389095dc0221468f024ee1ff8d15967
BrandonHaslam <BrandonHaslam@users.noreply.github.com>
2022-03-01 00:00:17 +0000

    fucking last time

--- components/layout/Logo.vue
+++ components/layout/Logo.vue
@@ -1,7 +1,7 @@
 <template>
     <NuxtLink class="!p-0" to="/">
         <!-- this needs changing to text -->
-        <img class="h-40 w-full" src="@@/assets/images/CircleLogo.svg" alt="" />
+        <img class="h-40 w-full" src="@/assets/images/CircleLogo.svg" alt="" />
     </NuxtLink>
 </template>
 <script>
